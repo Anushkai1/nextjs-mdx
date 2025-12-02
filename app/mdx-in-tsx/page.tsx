64a7c89121fd8e4e4d064d32b34ef3131460b367
Anushka Isuru <129645615+Anushkai1@users.noreply.github.com>
2025-12-03 00:05:20 +0530

    .mdx Component use in .tsx

---
+++ app/mdx-in-tsx/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import MdxComponent from '../../components/mdx-component.mdx'
+
+function Page() {
+    return (
+        <div>
+            <h1>Mdx in Tsx</h1>
+            <MdxComponent/>
+        </div>
+    )
+}
+
+export default Page
